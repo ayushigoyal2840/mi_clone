771513ca8a2e7c304887582dd2df9c5101124cc7
Ayushi Goel <ayushi1911012@akgec.ac.in>
2022-04-27 23:29:02 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App" style= {{
+      backgroundColor: "#cccc",
+      height: "100px",
+      color: 'yellow'
+}}>
+      <h1>hello</h1>
+      
+    </div>
+  );
+}
+
+export default App;
