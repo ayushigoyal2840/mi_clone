dda1fb359cd880aa1cecc5c7eb5a200a29974ad2
Ayushi Goel <ayushi1911012@akgec.ac.in>
2022-04-29 00:18:52 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar.js';
+import Slider from './components/Slider.js';
 import {BrowserRouter as Router } from 'react-router-dom';
+import data from './data/data.json'
+// const banner=pkg.banner;
+
+
 
 function App() {
   return (
    <Router>
      <PreNavbar />
      <Navbar />
+     <Slider start={data.banner.start} />
    </Router>
   );
 }
