ae7b2604a4d5cd42b218c52373cd5d63aac9053b
Ayushi Goel <ayushi1911012@akgec.ac.in>
2022-04-29 18:02:03 +0530

    new section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar.js';
 import Slider from './components/Slider.js';
 import {BrowserRouter as Router } from 'react-router-dom';
-import data from './data/data.json'
+import data from './data/data.json';
+import Offers from './components/Offers.js';
+import Heading from './components/Heading.js';
+import StarProduct from "./components/StarProduct.js"
+
 // const banner=pkg.banner;
 
 
@@ -15,6 +19,9 @@ function App() {
      <PreNavbar />
      <Navbar />
      <Slider start={data.banner.start} />
+     <Offers offer={data.offer} />
+     <Heading text="STAR PRODUCTS"/>
+     <StarProduct starProduct={data.starProduct} />
    </Router>
   );
 }
